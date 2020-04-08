e8f31134d26945dce5bf609bee8a60e3acfa269d
Anubhav Saxena <tuthubpro@gmail.com>
2020-04-09 05:04:28 +0530

    tile chart data and mapoing done

--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,50 +1,34 @@
 import React, {Component} from 'react';
-// import L from 'leaflet';
-import WorldMap from './map.component';
-// import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
 import axios from 'axios';
+import Chart from "react-google-charts";
 import CustomNavbar from "./navbar.component";
-import { MDBBox, MDBRow, MDBCol, MDBTable,
-  MDBTableBody,MDBTableHead,
-      MDBCardImage,MDBCard, MDBCardHeader, MDBCardBody } from 'mdbreact';
+import { MDBBox, MDBRow } from 'mdbreact';
 
-
-// var myIcon = L.icon({
-//   iconUrl: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABkAAAApCAYAAADAk4LOAAAFgUlEQVR4Aa' +
-//       '1XA5BjWRTN2oW17d3YaZtr2962HUzbDNpjszW24mRt28p47v7zq/bXZtrp/lWnXr337j3nPCe85Ncypg' +
-//       'SFdugCpW5YoDAMRaIMqRi6aKq5E3YqDQO3qAwjVWrD8Ncq/RBpykd8oZUb/kaJutow8r1aP9II0WmLKL' +
-//       'IsJyv1w/kqw9Ch2MYdB++12Onxee/QMwvf4/Dk/Lfp/i4nxTXtOoQ4pW5Aj7wpici1A9erdAN2OH64x8' +
-//       'OSP9j3Ft3b7aWkTg/Fm91siTra0f9on5sQr9INejH6CUUUpavjFNq1B+Oadhxmnfa8RfEmN8VNAsQhPq' +
-//       'F55xHkMzz3jSmChWU6f7/XZKNH+9+hBLOHYozuKQPxyMPUKkrX/K0uWnfFaJGS1QPRtZsOPtr3NsW0uy' +
-//       'h6NNCOkU3Yz+bXbT3I8G3xE5EXLXtCXbbqwCO9zPQYPRTZ5vIDXD7U+w7rFDEoUUf7ibHIR4y6bLVPXr' +
-//       'z8JVZEql13trxwue/uDivd3fkWRbS6/IA2bID4uk0UpF1N8qLlbBlXs4Ee7HLTfV1j54APvODnSfOWBq' +
-//       'tKVvjgLKzF5YdEk5ewRkGlK0i33Eofffc7HT56jD7/6U+qH3Cx7SBLNntH5YIPvODnyfIXZYRVDPqgHt' +
-//       'Ls5ABHD3YzLuespb7t79FY34DjMwrVrcTuwlT55YMPvOBnRrJ4VXTdNnYug5ucHLBjEpt30701A3Ts+H' +
-//       'Ea73u6dT3FNWwflY86eMHPk+Yu+i6pzUpRrW7SNDg5JHR4KapmM5Wv2E8Tfcb1HoqqHMHU+uWDD7zg54' +
-//       'mz5/2BSnizi9T1Dg4QQXLToGNCkb6tb1NU+QAlGr1++eADrzhn/u8Q2YZhQVlZ5+CAOtqfbhmaUCS1ez' +
-//       'NFVm2imDbPmPng5wmz+gwh+oHDce0eUtQ6OGDIyR0uUhUsoO3vfDmmgOezH0mZN59x7MBi++WDL1g/eE' +
-//       'iU3avlidO671bkLfwbw5XV2P8Pzo0ydy4t2/0eu33xYSOMOD8hTf4CrBtGMSoXfPLchX+J0ruSePw3LZ' +
-//       'eK0juPJbYzrhkH0io7B3k164hiGvawhOKMLkrQLyVpZg8rHFW7E2uHOL888IBPlNZ1FPzstSJM694fWr' +
-//       '6RwpvcJK60+0HCILTBzZLFNdtAzJaohze60T8qBzyh5ZuOg5e7uwQppofEmf2++DYvmySqGBuKaicF1b' +
-//       'lQjhuHdvCIMvp8whTTfZzI7RldpwtSzL+F1+wkdZ2TBOW2gIF88PBTzD/gpeREAMEbxnJcaJHNHrpzji' +
-//       '0gQCS6hdkEeYt9DF/2qPcEC8RM28Hwmr3sdNyht00byAut2k3gufWNtgtOEOFGUwcXWNDbdNbpgBGxEv' +
-//       'KkOQsxivJx33iow0Vw5S6SVTrpVq11ysA2Rp7gTfPfktc6zhtXBBC+adRLshf6sG2RfHPZ5EAc4sVZ83' +
-//       'yCN00Fk/4kggu40ZTvIEm5g24qtU4KjBrx/BTTH8ifVASAG7gKrnWxJDcU7x8X6Ecczhm3o6YicvsLXW' +
-//       'fh3Ch1W0k8x0nXF+0fFxgt4phz8QvypiwCCFKMqXCnqXExjq10beH+UUA7+nG6mdG/Pu0f3LgFcGrl2s' +
-//       '0kNNjpmoJ9o4B29CMO8dMT4Q5ox8uitF6fqsrJOr8qnwNbRzv6hSnG5wP+64C7h9lp30hKNtKdWjtdkb' +
-//       'uPA19nJ7Tz3zR/ibgARbhb4AlhavcBebmTHcFl2fvYEnW0ox9xMxKBS8btJ+KiEbq9zA4RthQXDhPa0T' +
-//       '9TEe69gWupwc6uBUphquXgf+/FrIjweHQS4/pduMe5ERUMHUd9xv8ZR98CxkS4F2n3EUrUZ10EYNw7BW' +
-//       'm9x1GiPssi3GgiGRDKWRYZfXlON+dfNbM+GgIwYdwAAAAASUVORK5CYII=',
-//   iconSize: [
-//     25, 41
-//   ],
-//   iconAnchor: [
-//     12.5, 41
-//   ],
-//   popupAnchor: [0, -41]
-// });
 export default class Home extends Component {
-
+  object = {
+    totalCases:0,
+    totalActive:0,
+    totalDeath:0,
+    totalRecovered:0,
+    color:{
+      deathColor:'#CC0000',
+      activeColor:'#FF8800',
+      recoveredColor:'#007E33',
+      totalColor:'#0099CC'
+    }
+  }
+titleChartTotalCasesData = [
+  ['x', 'Total cases']
+];
+titleChartDailyRecoveredData = [
+  ['x', 'Total Recovered']
+];
+titleChartDailyDeathData = [
+  ['x', 'Total Death']
+];
+titleChartDailyActiveData = [
+  ['x', 'Total Active']
+];
   state = {
     currentLocation: {
     lat : 10,
@@ -94,8 +78,10 @@ this.rowClickHandler = this.rowClickHandler.bind(this);
       this.setState({districtApiResponse:res1.data});
       
       axios.get(`https://api.covid19india.org/data.json`).then(res2 => {
-        this.setState({stateApiResponse: res2.data});
         
+        
+        this.setState({stateApiResponse: res2.data});
+        this.tileChartData(res2.data);
         Object.entries(res1.data).forEach(([k1,v1]) => {
           
           Object.entries(res2.data.statewise).forEach(([k2,v2]) => {
@@ -164,8 +150,28 @@ this.rowClickHandler = this.rowClickHandler.bind(this);
     this.setState({markerPopup: makerPopup});
     this.setState({currentLocation: location});
   }
+
+tileChartData(res){
+
+
+ res.cases_time_series.map((item,key)=> {
+   this.object.totalCases = +parseInt(item.totalconfirmed);
+   this.object.totalActive = +(parseInt(item.totalconfirmed) - parseInt(item.totalrecovered) - parseInt(item.totaldeceased));
+this.object.totalDeath = +parseInt(item.totaldeceased);
+   this.object.totalRecovered = +parseInt(item.totalrecovered);
+this.titleChartTotalCasesData.push([key, parseInt(item.totalconfirmed)]);
+this.titleChartDailyDeathData.push([key,parseInt(item.totaldeceased)])
+this.titleChartDailyRecoveredData.push([key,parseInt(item.totalrecovered)])
+this.titleChartDailyActiveData.push([key, (parseInt(item.totalconfirmed) - parseInt(item.totalrecovered) - parseInt(item.totaldeceased))])
+});
+console.log(this.state.stateApiResponse);
+
+
+}
+
   render() {
   this.geoCoder();
+
   const tableRows = this.state.completeInfo.map((val,index) => {
 return ( <tr key={index} onClick={() => this.rowClickHandler(index, val)}>
           <td>{index+1}</td>
@@ -179,51 +185,238 @@ return ( <tr key={index} onClick={() => this.rowClickHandler(index, val)}>
 
 
     return (
-      <div>
+      <MDBBox  tag="div">
       <CustomNavbar/>
       
 <MDBBox className="container-fluid" style = {{ width: "100%", marginTop: "1%" }} >
-<MDBRow>
-        <MDBCol md="6">
-              <MDBCard narrow>
-                <MDBCardImage className='view view-cascade gradient-card-header purple-gradient' cascade tag='div'>
-                    <MDBCardHeader tag='h4' className='text-center'>Statistics of India of Covid 19</MDBCardHeader>
-                </MDBCardImage>
-                <MDBCardBody cascade className='text-center'>
-                <div className="table-responsive">
-                  <MDBTable className="table" >
-                    <MDBTableHead>
-                      <tr>
-                        <th>#</th>
-                        <th>State/UT</th>
-                        <th>Confirmed Cases</th>
-                        <th>Active Cases</th>
-                        <th>Recovered Cases</th>
-                        <th>Deceased Cases</th>
-                      </tr>
-                    </MDBTableHead>
-                    <MDBTableBody>
-                    {
-                      tableRows
-                    }
-                    </MDBTableBody>
-                  </MDBTable>
-                </div>
-                  </MDBCardBody>
-              </MDBCard>  
-        </MDBCol>
-        <MDBCol md="6">
+
           <MDBRow>
-              <MDBBox tag="div" className="col-sm">
-              <WorldMap currentLocation={this.state.currentLocation} markerPopup={this.state.markerPopup}/>
-              
-              </MDBBox>
+          <div className="col-12 col-lg-6 col-xl">
+
+            
+            <div className="card">
+              <div className="card-body">
+                <div className="row align-items-center">
+                  <div className="col">
+
+                    
+                    <h6 className="text-uppercase mb-2" style={{color: "#0099CC"}}>
+                      Total Cases
+                    </h6>
+
+                    
+< span className = "h2 mb-0" style={{color: "#0099CC"}} >
+                     {this.object.totalCases}
+                    </span>
+
+                    
+                    
+                  </div>
+                  <div className="col-auto">
+
+                    
+                    <span className="h2 fe fe-dollar-sign text-muted mb-0">
+                    <Chart
+                        width={'100px'}
+                        height={'100px'}
+                        chartType="LineChart"
+                        loader={<div>Loading Chart</div>}
+                        data={
+                          this.titleChartTotalCasesData
+                        }
+                        options={{
+                          hAxis: {
+                            title: 'Day',
+                          },
+                          vAxis: {
+                            title: 'Cases',
+                          },
+                          series: {
+                            1: { curveType: 'function' },
+                          },
+                          colors: ["#33b5e5"]
+                        }}
+                        rootProps={{ 'data-testid': '1' }}
+                      />
+                    </span>
+
+                  </div>
+                </div> 
+              </div>
+            </div>
+
+          </div>
+          <div className="col-12 col-lg-6 col-xl">
+
+            
+            <div className="card">
+              <div className="card-body">
+                <div className="row align-items-center">
+                  <div className="col">
+
+                    
+                    <h6 className="text-uppercase mb-2" style={{color: '#CC0000'}}>
+                      Total Deaths
+                    </h6>
+
+                    
+< span className = "h2 mb-0" style = {{color: '#CC0000'}} >
+                     {this.object.totalDeath}
+                    </span>
+
+                    
+                    
+                  </div>
+                  <div className="col-auto">
+
+                    
+                    <span className="h2 fe fe-dollar-sign text-muted mb-0">
+                    <Chart
+                        width={'100px'}
+                        height={'100px'}
+                        chartType="LineChart"
+                        loader={<div>Loading Chart</div>}
+                        data={
+                          this.titleChartDailyDeathData
+                        }
+                        options={{
+                          hAxis: {
+                            title: 'Day',
+                          },
+                          vAxis: {
+                            title: 'Cases',
+                          },
+                          series: {
+                            1: { curveType: 'function' },
+                          },
+                          colors: ["#ff4444"]
+                        }}
+                        rootProps={{ 'data-testid': '1' }}
+                      />
+                    </span>
+
+                  </div>
+                </div> 
+              </div>
+            </div>
+
+          </div>
+          <div className="col-12 col-lg-6 col-xl">
+
+            
+            <div className="card">
+              <div className="card-body">
+                <div className="row align-items-center">
+                  <div className="col">
+
+                    
+                    <h6 className="text-uppercase mb-2" style={{color: '#007E33'}}>
+                      Total Recovered Cases
+                    </h6>
+
+                    
+                    <span className="h2 mb-0" style={{color: '#007E33'}}>
+                     {this.object.totalRecovered}
+                    </span>
+
+                    
+                    
+                  </div>
+                  <div className="col-auto">
+
+                    
+                    <span className="h2 fe fe-dollar-sign text-muted mb-0">
+                    <Chart
+                        width={'100px'}
+                        height={'100px'}
+                        chartType="LineChart"
+                        loader={<div>Loading Chart</div>}
+                        data={
+                          this.titleChartDailyRecoveredData
+                        }
+                        options={{
+                          hAxis: {
+                            title: 'Day',
+                          },
+                          vAxis: {
+                            title: 'Cases',
+                          },
+                          series: {
+                            1: { curveType: 'function' },
+                          },
+                          colors: ["#00C851"]
+                        }}
+                        rootProps={{ 'data-testid': '1' }}
+                      />
+                    </span>
+
+                  </div>
+                </div> 
+              </div>
+            </div>
+
+          </div>
+          <div className="col-12 col-lg-6 col-xl">
+
+            
+            <div className="card">
+              <div className="card-body">
+                <div className="row align-items-center">
+                  <div className="col">
+
+                    
+                    <h6 className="text-uppercase mb-2" style={{color: '#FF8800'}}>
+                      Total Active Cases
+                    </h6>
+
+                    
+                    <span className="h2 mb-0" style={{color: '#FF8800'}}>
+                     {this.object.totalActive}
+                    </span>
+
+                    
+                    
+                  </div>
+                  <div className="col-auto">
+
+                    
+                    <span className="h2 fe fe-dollar-sign text-muted mb-0">
+                    <Chart
+                        width={'100px'}
+                        height={'100px'}
+                        chartType="LineChart"
+                        loader={<div>Loading Chart</div>}
+                        data={
+                          this.titleChartDailyActiveData
+                        }
+                        options={{
+                          hAxis: {
+                            title: 'Day',
+                          },
+                          vAxis: {
+                            title: 'Cases',
+                          },
+                          series: {
+                            1: { curveType: 'function' },
+                          },
+                          colors: ["#ffbb33"]
+                        }}
+                        rootProps={{ 'data-testid': '1' }}
+                      />
+                    </span>
+
+                  </div>
+                </div> 
+              </div>
+            </div>
+
+          </div>
+          
           </MDBRow>
-        </MDBCol>
-    </MDBRow>
-        </MDBBox> 
+</MDBBox>
+    
         
-    </div>
+    </MDBBox >
     );
   }
 }
