d3468533b8116af552ad6645f09bc9af1d6cf1ff
Anubhav Saxena <tuthubpro@gmail.com>
2020-04-12 03:20:03 +0530

    removed

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="globals/axios/index.d.ts" />
 /// <reference path="globals/bootstrap/index.d.ts" />
 /// <reference path="globals/firebase/index.d.ts" />
-/// <reference path="globals/leaflet/index.d.ts" />
 /// <reference path="globals/react-router-dom/index.d.ts" />
